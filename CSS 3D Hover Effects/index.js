6c5c3453afac27f3183d68eaec1f989fd6828d5c
Josue Gatica Odato <josuegaticaodato@gmail.com>
2025-02-11 12:59:21 -0300

    11/2 - CSS 3D Hover Effects

---
+++ CSS 3D Hover Effects/index.js
@@ -0,0 +1,14 @@
+document.querySelectorAll(".box").forEach((box , index, boxes ) => {
+  box.addEventListener("mouseover", () => {
+    boxes.forEach((b,i) => {
+      let className = i < index ? "prev" : i === index ? "hovered" : "next";
+      b.classList.remove("prev","next","hovered")
+      b.classList.add(className)
+    })
+  })
+  box.addEventListener("mouseleave", () => {
+    boxes.forEach((b) => {
+      b.classList.remove("prev","next","hovered")
+    })
+  })
+})
